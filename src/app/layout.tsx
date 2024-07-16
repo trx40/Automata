3639520fb5aaf414b27b080dde2af4eefc84f31e
Tejas Mahajan <trm2468@gmail.com>
2024-07-16 20:23:14 +0530

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "./components/theme-provider";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Automata",
+  description: "Automate your work with Automata",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Navbar from "./components/global/Navbar";
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
+}
