b9277e802b4b39df1260ee52e0cefa52debce3c4
Tejas Mahajan <trm2468@gmail.com>
2024-07-17 12:47:03 +0530

    Add tailwind utils

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { ThemeProvider } from "./components/theme-provider";
+import { ThemeProvider } from "../components/theme-provider";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
