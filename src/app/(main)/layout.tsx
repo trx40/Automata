48618719fc8f22169236511593cdf04b3ab3c27d
Tejas Mahajan <trm2468@gmail.com>
2024-07-18 14:41:27 +0530

    Sidebar and settings page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+import InfoBar from "@/components/infobar";
+import Sidebar from "@/components/sidebar";
 import React from "react";
 
 type Props = { children: React.ReactNode };
@@ -5,7 +7,11 @@ type Props = { children: React.ReactNode };
 const Layout = (props: Props) => {
   return (
     <div className="flex overflow-hidden h-screen">
-      <div className="w-full">{props.children}</div>
+      <Sidebar />
+      <div className="w-full">
+        <InfoBar />
+        {props.children}
+      </div>
     </div>
   );
 };
